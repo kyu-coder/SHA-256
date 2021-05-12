4d01a2d56a87e0c005078611c369dd28da5d93b3
kyu <kyu.fastai@gmail.com>
2021-05-12 01:51:53 -0500

    deleted shift operator and now using string slice/concat for binary shift/rotation

--- helper.js
+++ helper.js
@@ -4,26 +4,24 @@ const displayContainer = document.querySelector(".show"),
     SHR_n_display = displayContainer.querySelector("#SHR_n"),
     ROTR_n_display = displayContainer.querySelector("#ROTR_n");
 
-
+const INT_BITS = 32;
 
 function getInputValue(){
     var inputVal = +document.getElementById("input").value;
-    var inputVal_binary = inputVal.toString(2);
-    globalThis.INT_BITS = inputVal_binary.length;
+    var inputVal_binary = inputVal.toString(2).padStart(INT_BITS, '0');
     var n = +document.getElementById("input_n").value;
 
     decimal.innerText = inputVal;
     binary.innerText = inputVal_binary;
-    SHR_n_display.innerText = SHR_n(inputVal, n);
-    ROTR_n_display.innerText = ROTR_n(inputVal, n);
+    SHR_n_display.innerText = SHR_n(inputVal_binary, n);
+    ROTR_n_display.innerText = ROTR_n(inputVal_binary, n);
     
 }
 
 function SHR_n(input, n){
-    return ( (input >> n).toString(2).substr(-INT_BITS) );
+    return ( input.slice(0, -n).padStart(INT_BITS, '0') );
 }
 
-function ROTR_n(input, n){  
-    return ( ((input >> n ) | (input << (INT_BITS - n))).toString(2).substr(-INT_BITS) );
+function ROTR_n(input, n){
+    return ( input.slice(-n) + input.slice(0, -n) );
 }
-
