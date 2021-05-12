a52213e6fa1bc0a3f354a9d266513fcfd7a9f092
kyu <kyu.fastai@gmail.com>
2021-05-12 12:11:31 -0500

    display update

--- helper.js
+++ helper.js
@@ -1,20 +1,25 @@
 const displayContainer = document.querySelector(".show"),
     decimal = displayContainer.querySelector("#decimal"),
     binary = displayContainer.querySelector("#binary"),
-    SHR_n_display = displayContainer.querySelector("#SHR_n"),
-    ROTR_n_display = displayContainer.querySelector("#ROTR_n");
+    sigma_0_display = displayContainer.querySelector("#sigma_0"),
+    sigma_1_display = displayContainer.querySelector("#sigma_1");
+    sigma_upper_0_display = displayContainer.querySelector("#sigma_upper_0");
+    sigma_upper_1_display = displayContainer.querySelector("#sigma_upper_1");
 
 const INT_BITS = 32;
 
 function getInputValue(){
+
     var inputVal = +document.getElementById("input").value;
     var inputVal_binary = inputVal.toString(2).padStart(INT_BITS, '0');
-    var n = +document.getElementById("input_n").value;
 
     decimal.innerText = inputVal;
     binary.innerText = inputVal_binary;
-    SHR_n_display.innerText = SHR_n(inputVal_binary, n);
-    ROTR_n_display.innerText = ROTR_n(inputVal_binary, n);
+    sigma_0_display.innerText = sigma_0(inputVal);
+    sigma_1_display.innerText = sigma_1(inputVal);
+    sigma_upper_0_display.innerText = sigma_upper_0(inputVal);
+    sigma_upper_1_display.innerText = sigma_upper_1(inputVal);
+    
     
 }
 
@@ -44,6 +49,23 @@ function sigma_1(input){
     return binary_solution;
 }
 
+function sigma_upper_0(input){
+    
+    var decimal = sigma_upper_n(input, 2, 13, 22);
+
+    var binary_solution = decimal_to_unsignedTwosComplement(decimal);
+
+    return binary_solution;
+}
+
+function sigma_upper_1(input){
+    
+    var decimal = sigma_upper_n(input, 6, 11, 25);
+
+    var binary_solution = decimal_to_unsignedTwosComplement(decimal);
+
+    return binary_solution;
+}
 
 function sigma_n(input, a, b, c){
     input_binary = input.toString(2).padStart(INT_BITS, '0');
@@ -52,9 +74,21 @@ function sigma_n(input, a, b, c){
     const ROTR_b = parseInt(ROTR_n(input_binary, b), 2);
     const SHR_c = parseInt(SHR_n(input_binary, c), 2);
 
-    var binary_solution = ( ROTR_a ^ ROTR_b ) ^ SHR_c;
+    var decimal_solution = ( ROTR_a ^ ROTR_b ) ^ SHR_c;
 
-    return binary_solution;
+    return decimal_solution;
+}
+
+function sigma_upper_n(input, a, b, c){
+    input_binary = input.toString(2).padStart(INT_BITS, '0');
+
+    const ROTR_a = parseInt(ROTR_n(input_binary, a), 2);
+    const ROTR_b = parseInt(ROTR_n(input_binary, b), 2);
+    const ROTR_c = parseInt(ROTR_n(input_binary, c), 2);
+
+    var decimal_solution = ( ROTR_a ^ ROTR_b ) ^ ROTR_c;
+
+    return decimal_solution;
 }
 
 function decimal_to_unsignedTwosComplement(decimal){
